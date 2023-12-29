a8d1077249cf1cc1c49ddbed9af34d79ddccac35
Tosinkuzzy <Adeleye608@gmail.com>
2023-12-29 02:55:58 +0100

    aliasing modified

--- pCA_files/progs/main2.py
+++ pCA_files/progs/main2.py
@@ -1,10 +1,10 @@
 from sys import path
 path.append('..//packages')
 
-import extra.good.best.sigma
+import extra.good.best.sigma as sig
 from extra.good.best.tau import funcT
 from extra.iota import funcI
 
-print(extra.good.best.sigma.funcS())
+print(sig.funcS())
 print(funcT())
 print(funcI())

193b1ca5501ed46cd3d6a22190a80ff90333a4fb
Tosinkuzzy <Adeleye608@gmail.com>
2023-12-29 03:24:24 +0100

    MODULES AND PACKAGES SUCCESSFUL

--- pCA_files/progs/main2.py
+++ pCA_files/progs/main2.py
@@ -2,9 +2,13 @@ from sys import path
 path.append('..//packages')
 
 import extra.good.best.sigma as sig
+import extra.good.alpha as alp
+from extra.good.beta import funcB
 from extra.good.best.tau import funcT
 from extra.iota import funcI
 
 print(sig.funcS())
+print(alp.funcA())
+print(funcB())
 print(funcT())
 print(funcI())
